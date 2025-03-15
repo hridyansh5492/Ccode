6570cb58c32bffa5e93bc5f58061d47608da2593
hridyansh5492 <hridyanshsati4321@gmail.com>
2025-03-15 16:54:06 +0530

    Second file

---
+++ comEsq.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // About comments and escape sequences in C.
+    //Simple way to sum values in C.
+    int a = 5, b = 10;
+    printf("The value of  a+b is %d.\n", a+b);
+    /*
+    This is example of multi line comment.
+    
+    Escape sequences are character combination consisting of backslash (\) followed by a letter or combination of digits.
+    Used here is /n which is used for newline.
+
+    Others are /t for tab, /b for backspace, /r for carriage return, /f for form feed, /' for single quote, /" for double quote, /? for question mark, /0 for null character.
+    */
+    return 0;
+}
