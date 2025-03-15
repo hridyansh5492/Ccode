b521a109faf6bc7489b72178baf8ed9a875275e7
hridyansh5492 <hridyanshsati4321@gmail.com>
2025-03-15 19:49:27 +0530

    4th time commiting this day

---
+++ const.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    //Constants are variables whose values cannot be changed during its execution.
+    const float PI = 3.14159;
+    printf("PI = %f\n", PI);
+
+    return 0;
+}
