9fae8edcd20a4cf87bebfeb116f5d6a83c476f18
hridyansh5492 <hridyanshsati4321@gmail.com>
2025-08-29 10:08:44 +0530

    patt file

---
+++ patt.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=3;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 1; j <= a; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+    
+}
